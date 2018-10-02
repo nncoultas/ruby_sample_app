725da0db073cea79e0086400df645673fa1eada1
Nick Coultas <nncoultas@yahoo.com>
2018-10-02 12:57:20 -0700

    added layout changes and started the process of users being able to login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,18 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create 
-    render 'new'
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
   
   def destroy
   end
-  
+
 end
