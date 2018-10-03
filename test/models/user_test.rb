f3e7c84b4fcb0844641599dd1a412baec1c42425
Nick Coultas <nncoultas@yahoo.com>
2018-10-03 11:10:49 -0700

    fixed bug if user logs out in another window

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -21,6 +21,10 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?("")
+  end
+
   test "password should be present (nonblank)" do
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
